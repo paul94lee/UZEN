6cf0e3381132dd8eeac20bd7ee9c71fa253d311c
graphicnovel <onegoseen@gmail.com>
2020-12-02 10:53:53 +0900

    main 반응형
    main 반응형

--- js/header.js
+++ js/header.js
@@ -3,9 +3,12 @@ window.addEventListener('load', function () {
     var btn = document.querySelector('.bugerBtn');
     var hid = document.querySelector('.hidden-menu');
     console.log(btn)
-    btn.addEventListener('click', function () {
-        btn.classList.toggle('active');
-        hid.classList.toggle('active');
-        
-    });
+    setTimeout(() => {
+        btn.addEventListener('click', function () {
+            btn.classList.toggle('active');
+            hid.classList.toggle('active');
+
+        });
+    }, 100);
+
 });
